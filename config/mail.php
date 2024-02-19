1618271a8221c5215dfa54fad2e307242c685246
Segz <segz@Segzs-MacBook-Pro.local>
2024-02-19 05:06:51 +0100

    A

--- config/mail.php
+++ config/mail.php
@@ -34,51 +34,6 @@ return [
     */
 
     'mailers' => [
-
-        "mailjet" => [
-
-            "key" => env("MAILJET_APIKEY"),
-
-            "secret" => env("MAILJET_APISECRET"),
-
-            "transactional" => [
-
-                "call" => true,
-
-                "options" => [
-
-                    "url" => "api.mailjet.com",
-
-
-                    "version" => "v3.1",
-
-                    "call" => true,
-
-                    "secured" => true
-
-                ]
-
-            ],
-
-            "common" => [
-
-                "call" => true,
-
-                "options" => [
-
-                    "url" => "api.mailjet.com",
-
-                    "version" => "v3",
-
-                    "call" => true,
-
-                    "secured" => true
-
-                ]
-
-            ]
-                ],
-
         'smtp' => [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
